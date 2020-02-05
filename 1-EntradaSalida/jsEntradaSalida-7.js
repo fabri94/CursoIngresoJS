0b0bb925c9eda6931e9e7d7e2a294889eda9af25
fabri94 <60664124+fabri94@users.noreply.github.com>
2020-02-05 10:23:29 -0300

    Ejercicio 7 (entrada y salida de datos) solo suma
    Falta hacer resta, multiplicacion y division.

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,12 +4,32 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;//Lo primero que se hace es definir las variables arriba, asignandole nombres acorde a la necesidad
+	var segundoNumero;
+	var resultadoSuma;//Se define una variable resultado tambien que implique una operacion aritmetica entre ambos numeros.
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+
+	resultadoSuma=primerNumero+segundoNumero;
+	console.log(resultadoSuma);
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoResta;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	resultadoResta=primerNumero - segundoNumero;
+	console.log(resultadoResta);
 }
 
 function multiplicar()
