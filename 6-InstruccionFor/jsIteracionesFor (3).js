2a44ac5a4d5608849f4bcb26c98fe3157b8fc540
alumno <alumno>
2020-02-26 12:06:06 -0300

    Commit: Iteraciones For

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	var repeticiones=prompt("ingrese el número de repeticiones");
+	var contador;
+	contador=parseInt(contador);
+
+	for (contador=0;contador<repeticiones;contador++)
+	{
+		console.log("Hola UTN fra");
+	}
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,26 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var i;
+	var bandera;
+
+	bandera=0
+	numeroIngresado=prompt("Ingrese el numero ");
+	numeroIngresado=parseInt(numeroIngresado);
 
 
+	for(i=2;i<numeroIngresado;i++)
+	{
+		console.log(i);
+		if(numeroIngresado%i==0) //esto quiere decir que el numeroIngresado es divisible por el i. Si encontre un divisor quiere decir que ya no es primo.
+		{
+			bandera=1;
+			//break;
+		} 
+	}
 
-
+	if(bandera==0)
+	{
+		alert("Es primo el numero: " +numeroIngresado);
+	}
 }//FIN DE LA FUNCIÓN
