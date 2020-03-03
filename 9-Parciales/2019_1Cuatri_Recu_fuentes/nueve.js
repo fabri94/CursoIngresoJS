4e6bc6427f5405b05fcd968683eba47b54d7bd00
fabri94 <fabrinf1994@gmail.com>
2020-03-03 15:44:38 -0300

    Ejercicio 9 Recuperatorio Parcial TERMINADO

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -13,21 +13,46 @@ function mostrar()
 	var sexo;
 	var edad;
 	var notaFinal;
-	var contadorAprobados;
-	var contadorVaronesAprobados;
-	var contadorMujeresAprobadas;
-	var contadorNotasMenores;
-	var contadorNotasAdolescentes;
-	var contadorNotasMayores;
-
-	contadorAprobados=0;
-	contadorVaronesAprobados=0;
-	contadorMujeresAprobadas=0;
-	contadorNotasMenores=0;
-	contadorNotasAdolescentes=0;	
-	contadorNotasMayores=0;
-
 	var respuesta;
+
+	var contadorVaronesAprobados;
+
+	var contadorNotasMenores;
+	var acumuladorNotasMenores;
+	var promedioNotasMenores;
+
+	var contadorNotasAdolescentes;
+	var acumuladorNotasAdolescentes;
+	var promedioNotasAdolescentes;
+
+	var contadorNotasMayores;
+	var acumuladorNotasMayores;
+	var promedioNotasMayores;
+
+	var contadorNotasVarones;
+	var acumuladorNotasVarones;
+	var promedioNotasVarones;
+
+	var contadorNotasMujeres;
+	var acumuladorNotasMujeres;
+	var promedioNotasMujeres;
+
+	contadorVaronesAprobados=0;
+	
+	contadorNotasMenores=0;
+	acumuladorNotasMenores=0;
+
+	contadorNotasAdolescentes=0;
+	acumuladorNotasAdolescentes=0;
+
+	contadorNotasMayores=0;	
+	acumuladorNotasMayores=0;
+
+	contadorNotasMujeres=0;
+	acumuladorNotasMujeres=0;
+
+	contadorNotasVarones=0;
+	acumuladorNotasVarones=0;
 
 	do
 	{
@@ -36,11 +61,13 @@ function mostrar()
 			nombre=prompt("Ingrese nombre.");
 		}while(!isNaN(nombre));
 
+		console.log("Nombre: "+nombre);
+
 		do
 		{
 			edad=prompt("Ingrese edad.");
 			edad=parseInt(edad);
-		}while(isNaN(edad)||edad<0 || edad>100);
+		}while(isNaN(edad)||edad<6 || edad>100);
 
 		console.log("Edad: "+edad);
 
@@ -55,28 +82,58 @@ function mostrar()
 		{
 			notaFinal=prompt("Ingrese la nota del final.");
 			notaFinal=parseInt(notaFinal);
-		}while(isNaN(notaFinal) || notaFinal<=0);
+		}while(isNaN(notaFinal) || notaFinal<0 || notaFinal>10);
 
 		console.log("Nota final: "+notaFinal);
 
-		if(sexo=="m" && notaFinal>=6)
+		if(sexo=="m" && notaFinal>5)
 		{
 			contadorVaronesAprobados++;
 		}
 
-		if(edad>17 && notaFinal>=6)
+		if(edad>17)
 		{
-
+			contadorNotasMayores++;
+			acumuladorNotasMayores=acumuladorNotasMayores+notaFinal;
+		}else
+		{
+			if(edad>12)
+			{
+				contadorNotasAdolescentes++;
+				acumuladorNotasAdolescentes=acumuladorNotasAdolescentes+notaFinal;
+			}else
+			{
+				contadorNotasMenores++;
+				acumuladorNotasMenores=acumuladorNotasMenores+notaFinal;
+			}
 		}
-
-
-
+		if(sexo=="f")
+		{
+			contadorNotasMujeres++;
+			acumuladorNotasMujeres=acumuladorNotasMujeres+notaFinal;
+		}else
+		{
+			contadorNotasVarones++;
+			acumuladorNotasVarones=acumuladorNotasVarones+notaFinal;
+		}
 
 
 		respuesta=prompt("¿Desea seguir ingresando datos?");
 
 	}while(respuesta =="si");
 
+	promedioNotasMenores=acumuladorNotasMenores/contadorNotasMenores;
+	promedioNotasAdolescentes=acumuladorNotasAdolescentes/contadorNotasAdolescentes;
+	promedioNotasMayores=acumuladorNotasMayores/contadorNotasMayores;
+	promedioNotasMujeres=acumuladorNotasMujeres/contadorNotasMujeres;
+	promedioNotasVarones=acumuladorNotasVarones/contadorNotasVarones;
+
+	document.write("<br>La cantidad de varones aprobados es: "+contadorVaronesAprobados);
+	document.write("<br>Promedio de notas de menores: "+promedioNotasMenores);
+	document.write("<br>Promedio de notas de adolescentes: "+promedioNotasAdolescentes);
+	document.write("<br>Promedio de notas de mayores: "+promedioNotasMayores);
+	document.write("<br>Promedio de notas de mujeres: "+promedioNotasMujeres);
+	document.write("<br>Promedio de notas de varones: "+promedioNotasVarones);
 
 }
 
